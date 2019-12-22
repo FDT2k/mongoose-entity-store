cac5d98a619a09d3f89d4c6fd325562610abafc2
Fabien Di Tore <fabien@ditore.ch>
2019-12-22 04:28:25 +0100

    wip

--- src/search_entity.js
+++ src/search_entity.js
@@ -6,6 +6,8 @@ export const make_language =  C.compose(C.as_prop('$language'),C.defaultTo('none
 
 //  Bool -> String -> String -> Object
 export const make_mongo_text_search = C.curry(($caseSensitive,$language,$search)=>({$text:{$search,...make_language($language),$caseSensitive}}))
+
+
 //  String -> Object
 export const make_mongo_text_projection = $meta => ({ score: { $meta }})
 
@@ -16,13 +18,13 @@ export const mongoose_default_search = C.curry((lang,term)=> make_mongo_text_sea
 
 
 /*
-  Using a search and a projection generating function
+  Using a search and a projection generating function: generate a task that perform the search_query
 
   MakeSearch = String -> String -> Object == Language -> SearchTerm -> MongoQuery
   MakeProjection = String -> Object == Meta -> MongoProjection
 */
 // FN -> FN -> MongooseModel -> Integer-> String -> String -> Promise
-const mongoose_text_search = C.curry(
+export const mongoose_text_search = C.curry(
   ( MakeSearch, MakeProjection,  Model, limit, lang, term  ) => {
     const projection = MakeProjection('textScore');
     const search_query = MakeSearch(lang,term);
@@ -30,27 +32,26 @@ const mongoose_text_search = C.curry(
   }
 )
 
+
+const defaultCatcher = Broker => Payload => Broker.replyIfNeeded(err);
+
+
+
 //  Settings -> Broker -> Payload
-export const search_entity = C.curry ((SearchTask,Broker,Payload)=>{
+/*
+SearchTask ::  String -> Task
+Broker
+Payload :: Object
+*/
+export const make_search_entity_service = C.curry ((SearchTask,payload,replyTo)=>{
   let response = {success:false}
-  let projection = make_mongo_text_projection('textScore');
-
-  try {
-    const {term} = validate(payload,['term']);
-
-    MongooseModel.find(make_mongo_text_search(false,'french',term),projection)
-      .sort(projection)
-      .limit(20)
-      .then((entities)=>{
-        response.success = true;
-        response[plural_entity_name] = entities;
-        Broker.replyIfNeeded(response)
-      }).catch(err=>{
-        Broker.replyIfNeeded(err);
-      });
-  } catch (err) {
-    Broker.replyIfNeeded(err);
-    return;
-  }
-
-})
+  const {term} = validate(payload,['term']);
+  SearchTask(term).fork(
+    (err)=>Broker.replyIfNeeded(err),
+    (entities)=>{
+      response.success = true;
+      response[plural_entity_name] = entities;
+      Broker.replyIfNeeded(response)
+    }
+  )
+});
--- tests/search_entity.test.js
+++ tests/search_entity.test.js
@@ -1,5 +1,5 @@
 
-import {make_mongo_text_search,make_mongo_text_projection,mongoose_default_search} from '../src/search_entity'
+import {make_mongo_text_search,make_mongo_text_projection,mongoose_default_search,mongoose_text_search} from '../src/search_entity'
 
 
 
@@ -7,7 +7,7 @@ test ('make mongo search', ()=>{
 
   expect(make_mongo_text_search(false)).toBeInstanceOf(Function)
   expect(make_mongo_text_search(false,null)).toBeInstanceOf(Function)
-   expect(make_mongo_text_search(false,'french')).toBeInstanceOf(Function)
+  expect(make_mongo_text_search(false,'french')).toBeInstanceOf(Function)
 
 
   expect (
@@ -69,5 +69,49 @@ test ('make mongo projection', ()=>{
 
   expect(make_mongo_text_projection('french')).toBeInstanceOf(Object)
   expect(make_mongo_text_projection('french')).toEqual({"score": {"$meta": "french"}})
+
+})
+
+
+test ('mongo search task', (done)=>{
+
+
+  let mockModel = _ =>( {
+    find:   (...args)=> {console.log(args); return mockModel()},
+    sort:   (sort)=>{ return mockModel()  },
+    limit:  (sort)=>{ return mockModel()  },
+    catch:  (_catch)=>{  return mockModel()  },
+    then:   (_then)=>{ _then({results:['a','b']}); return mockModel()  }
+  })
+
+  let test = mongoose_text_search(mongoose_default_search,make_mongo_text_projection,mockModel(),20,'french','test')
+
+  test.fork(
+    (res)=> console.log('error',res),
+    (res)=>{console.log(res); done()},
+  );
+
+})
+
+
+test ('mongo entity search service', (done)=>{
+
+
+  let mockModel = _ =>( {
+    find:   (...args)=> {console.log(args); return mockModel()},
+    sort:   (sort)=>{ return mockModel()  },
+    limit:  (sort)=>{ return mockModel()  },
+    catch:  (_catch)=>{  return mockModel()  },
+    then:   (_then)=>{ _then({results:['a','b']}); return mockModel()  }
+  })
+
+  let test = mongoose_text_search(mongoose_default_search,make_mongo_text_projection,mockModel(),20,'french','test')
+
+  test.fork(
+    (res)=> console.log('error',res),
+    (res)=>{console.log(res); done()},
+  );
+
+
 
 })
